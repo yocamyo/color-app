696d24aba111521d8fc87b849225e66bbba5c108
Camden Navis <naviscam@gmail.com>
2019-06-08 11:48:17 -0400

    Finish styling miniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from '@material-ui/styles';
 const styles = {
   root: {
     backgroundColor: 'blue',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center'
@@ -35,6 +35,7 @@ const styles = {
 class PaletteList extends Component {
   render() {
     const { palettes, classes } = this.props;
+
     return (
       <div className={classes.root}>
         <div className={classes.container}>
