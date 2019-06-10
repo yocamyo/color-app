503e744ac3b980da607c24cc674180868e61dd2f
Camden Navis <naviscam@gmail.com>
2019-06-10 18:11:15 -0400

    Updates

---
+++ precache-manifest.d8ba48f308870a16f37da24066eff650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "185c53cf20cf06d8c76b",
+    "url": "/yocamyo/color-app/static/css/main.51611fc0.chunk.css"
+  },
+  {
+    "revision": "185c53cf20cf06d8c76b",
+    "url": "/yocamyo/color-app/static/js/main.035df6d8.chunk.js"
+  },
+  {
+    "revision": "4c5993bbc20dbd12b731",
+    "url": "/yocamyo/color-app/static/js/runtime~main.55ad6c9c.js"
+  },
+  {
+    "revision": "4920e89c56e0fef5b845",
+    "url": "/yocamyo/color-app/static/css/2.5483facc.chunk.css"
+  },
+  {
+    "revision": "4920e89c56e0fef5b845",
+    "url": "/yocamyo/color-app/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/yocamyo/color-app/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "7a87ca7bf86a04ed3f5c9fb67f522145",
+    "url": "/yocamyo/color-app/index.html"
+  }
+];
