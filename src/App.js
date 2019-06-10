67d0fd44b21cb13c07e5481725cf78ee08097563
Camden Navis <naviscam@gmail.com>
2019-06-09 22:30:15 -0400

    Add SingleColorPalette and Link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -35,13 +36,9 @@ class App extends Component {
         <Route
           exact
           path='/palette/:paletteId/:colorId'
-          render={() => <h1>Single Color Page</h1>}
+          render={() => <SingleColorPalette />}
         />
       </Switch>
-
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
